373f1db33a8a06ba31e26288a33d0458057212e0
Kaloyan Drenski <drenski666@gmail.com>
2018-10-11 20:34:25 +0300

    Added localStorage

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,90 +1,111 @@
 import React, { Component } from "react";
-import TodoListItem from './TodoListItem';
+import TodoListItem from "./TodoListItem";
 
 class TodoList extends Component {
-  state = { 
-    items: [],
-    itemVal: '',
-    cssEditInput: 'displayNone'
-   }
-
-   handleItemValueChange = (e) => {
-    this.setState({itemVal: e.target.value})
+  constructor(props) {
+    super(props);
+    var storageArray;
   }
+
+  state = {
+    items: [],
+    itemVal: "",
+    cssEditInput: "displayNone"
+  };
+
+  handleItemValueChange = e => {
+    this.setState({ itemVal: e.target.value });
+  };
 
   handleItemCreateClick = () => {
     const items = [...this.state.items];
-    if (this.state.itemVal.length <= 0) {return null;}
+    if (this.state.itemVal.length <= 0) {
+      return null;
+    }
     items.push(this.state.itemVal);
 
     this.setState({
       items,
-      itemVal: '',
-    })
-  }
+      itemVal: ""
+    });
+  };
 
-  handleChange = (item,e) => {
-    console.log("SUP")
+  handleChange = (item, e) => {
+    console.log("SUP");
     const items = [...this.state.items];
     const key = items.indexOf(item);
     items[key] = e.target.value;
-    this.setState({items})
-  }
+    this.setState({ items });
+  };
 
-  handleItemEdit = (e) => {
-    this.setState({cssEditInput: 'form-control displayBlock'})
-  }
+  handleItemEdit = e => {
+    this.setState({ cssEditInput: "form-control displayBlock" });
+  };
 
   handleClickSave = (item, e) => {
     const items = [...this.state.items];
     const key = items.indexOf(item);
     items[key] = e;
-    this.setState({items, cssEditInput: 'displayNone'}, () => {console.log(this.state.items)});
-  }
+    this.setState({ items, cssEditInput: "displayNone" }, () => {
+      console.log(this.state.items);
+    });
+  };
 
-  handleDeleteItem = (item) => {
+  handleDeleteItem = item => {
     const items = [...this.state.items];
     const key = items.indexOf(item);
+
+    // Remove the item from localStorage
+    localStorage.removeItem(items[key]);
+
     items.splice(key, 1);
-    this.setState({items})
-  }
+    this.setState({ items });
+  };
 
   renderTodoItem(item) {
-    const randomKey = require('uuid/v4');
-    return <TodoListItem 
-    key={randomKey()} 
-    value={item} 
-    cssEditInput={this.state.cssEditInput}
-    onItemEditClick={this.handleItemEdit}
-    onClickSave={(e) => this.handleClickSave(item, e)}
-    onItemDeleteClick={() => this.handleDeleteItem(item)}
-    />;
+    const randomKey = require("uuid/v4");
+    return (
+      <TodoListItem
+        key={randomKey()}
+        value={item}
+        cssEditInput={this.state.cssEditInput}
+        onItemEditClick={this.handleItemEdit}
+        onClickSave={e => this.handleClickSave(item, e)}
+        onItemDeleteClick={() => this.handleDeleteItem(item)}
+      />
+    );
   }
 
-  render() { 
+  componentDidMount() {
+    this.storageArray = Object.values(localStorage);
+    this.setState({ items: this.storageArray });
+  }
 
-    return ( 
+  render() {
+    return (
       <div className="todo-list col-xs-12 col-lg-10 offset-1">
         <div className="todo-list-new-item">
           <div className="input-group mb-3">
-            <input 
-            type="text" 
-            value={this.state.itemVal} 
-            className="form-control input-new-item" 
-            placeholder="What you'll do later...?" 
-            aria-label="What you'll do later...?" 
-            aria-describedby="button-addon2"
-            onChange={(e) => {this.handleItemValueChange(e)}} 
+            <input
+              type="text"
+              value={this.state.itemVal}
+              className="form-control input-new-item"
+              placeholder="What you'll do later...?"
+              aria-label="What you'll do later...?"
+              aria-describedby="button-addon2"
+              onChange={e => {
+                this.handleItemValueChange(e);
+              }}
             />
 
             <div className="input-group-append">
-              <button 
-              className="btn btn-primary" 
-              type="button" 
-              id="button-addon2" 
-              onClick={this.handleItemCreateClick}
+              <button
+                className="btn btn-primary"
+                type="button"
+                id="button-addon2"
+                onClick={this.handleItemCreateClick}
               >
-              Create
+                Create
               </button>
             </div>
           </div>
@@ -92,8 +113,8 @@ class TodoList extends Component {
         <h1>My TODO List:</h1>
         {this.state.items.map(item => this.renderTodoItem(item))}
       </div>
-     );
+    );
   }
 }
- 
+
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (storageAvailable("localStorage")) {
+  // Yippee! We can use localStorage awesomeness
+  ReactDOM.render(<App />, document.getElementById("root"));
+} else {
+  // Too bad, no localStorage for us
+  alert("Your browser doesn't support localStorage :(!");
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function storageAvailable(type) {
+  try {
+    var storage = window[type],
+      x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0
+    );
+  }
+}
